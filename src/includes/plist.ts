6f293101808ebcee132866a5ad15d0d0783844fd
Cédric KETTERLIN <dark.ryujin@gmail.com>
2022-03-30 15:40:36 +0200

    minor corrections

--- src/includes/plist.ts
+++ src/includes/plist.ts
@@ -3,7 +3,7 @@ import * as plist from 'plist'
 
 export type Options = {[key: string]: boolean | string | string[]}
 
-export async function mergeEntries(filename: string, options: Options): Promise<void> {
+export function mergeEntries(filename: string, options: Options): void {
   const json = fs.existsSync(filename) ? JSON.parse(JSON.stringify(plist.parse(fs.readFileSync(filename, 'utf8')))) : {}
 
   fs.writeFileSync(filename, plist.build({...json, ...options}))
